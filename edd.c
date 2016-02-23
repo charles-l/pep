036e37a73e277bb2422724aad93714c4d7262db5
charlie <chuckiels2011@gmail.com>
2016-02-23 18:14:07 -0500

    added file reading

--- edd.c
+++ edd.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <ncurses.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+// TODO: use mmap to read portion of file rather than the entire stinkin' thing
 
 struct buf {
 	char *content;
@@ -13,8 +18,10 @@ void input(struct buf *b) {
 	{
 		case 'k':
 			b->line--;
+			break;
 		case 'j':
 			b->line++;
+			break;
 	}
 }
 
@@ -25,16 +32,22 @@ void display(struct buf *b) {
 int main(void) {
 	WINDOW *win;
 	if((win = initscr()) == NULL) {fprintf(stderr, "error initializing ncurses");}
+	noecho();
 	struct buf b = {NULL, NULL, 0};
+
+	int fd = open("./edd.c", O_RDONLY);
+	size_t len = lseek(fd, 0, SEEK_END);
+	char *file = (char *) mmap(0, len, PROT_READ, MAP_PRIVATE, fd, 0);
+
 	while(1) {
-		mvaddstr(0, 0, "content");
+		mvaddstr(0, 0, file);
+		move(b.line, 0);
 		refresh();
 		input(&b);
-		move(b.line, 0);
 	}
 
-	delwin(win);
 	endwin();
+	delwin(win);
 	refresh();
 	return 0;
 }
